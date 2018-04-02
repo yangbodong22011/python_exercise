1c401a0f2da97ebf574b73a174d6d7ab50525dcc
bodong.ybd <bodong.ybd@alibaba-inc.com>
2018-04-03 00:35:00 +0800

    ch07

---
+++ ch07/7-1.py
@@ -0,0 +1,18 @@
+# car = input("What car you wanna")
+# print(car)
+
+# people = input("How many people wanna to eat")
+# people = int(people)
+
+# if people > 8:
+# 	print("ok")
+# else:
+# 	print("no")
+
+num = input("Please input a number: ")
+num = int(num)
+
+if num%10 == 0:
+	print("the " + str(num) + " is yes ")
+else:
+	print("the " + str(num) + " is not")
